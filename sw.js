bf8474d4ca8c2d019056cddc65046995691f08bc
vitordalto <vitor.tecnolife@hotmail.com>
2026-03-26 18:43:03 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+var CACHE = 'jornada-motoboy-v1';
+var ARQUIVOS = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ARQUIVOS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
